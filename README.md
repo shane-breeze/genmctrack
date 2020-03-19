033d0883128e4061ac7184b5a6f84d2d3bc75cf0
Shane Breeze <s.breeze15@imperial.ac.uk>
2020-03-19 14:49:26 +0000

    Add LHE step

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # mctrack
-Instructions to generate MC for (HLT) tracking work
+Instructions to generate MC for (HLT) tracking work within the
+[CMSSW](https://github.com/cms-sw/cmssw) framework.
+
+Configs to generate each step of the MC chain can be found at
+[MCM](https://cms-pdmv.cern.ch/mcm/) by navigating through
+`requests > Navigation` and searching for the relevant sample. Generation of
+the ttbar for the 14 TeV HL-LHC upgrade will be included here as the stress
+test of the tracking algorithm.
+
+The various steps of generation are detailed below. Each step includes a setup
+bash script run using `sh <step>_setup.sh` which creates the CMSSW working area
+as well a the python config file run using `cmsRun <step>_cfg.py`.
+
+
+## LHE
+
+The LHE step involves three main sub-steps: lhe, gen, sim. The lhe and gen steps
+run the matrix element calculations, particle decay, hadronization and
+showering, which is subsequently stored in the
+[LHE format](https://arxiv.org/abs/hep-ph/0609017); the sim step involves the
+simulation of the CMS detector's response to the generated particles. This step
+creates an output `lhe.root` file with a temporary `lhe_inLHE.root` file.
---
+++ lhe_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_10_6_0_patch2/src ] ; then
+    echo release CMSSW_10_6_0_patch2 already exists
+else
+    scram p CMSSW CMSSW_10_6_0_patch2
+fi
+cd CMSSW_10_6_0_patch2/src
+eval `scram runtime -sh`
+
+curl -s --insecure https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/TSG-PhaseIITDRSpring19wmLHEGS-00030 --retry 2 --create-dirs -o Configuration/GenProduction/python/TSG-PhaseIITDRSpring19wmLHEGS-00030-fragment.py
+[ -s Configuration/GenProduction/python/TSG-PhaseIITDRSpring19wmLHEGS-00030-fragment.py ] || exit $?;
+
+scram b
+cd ../../
+seed=$(($(date +%s) % 100 + 1))
+cmsDriver.py Configuration/GenProduction/python/TSG-PhaseIITDRSpring19wmLHEGS-00030-fragment.py \
+        --fileout file:lhe.root \
+        --mc --eventcontent RAWSIM,LHE \
+        --customise SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000,Configuration/DataProcessing/Utils.addMonitoring \
+        --datatier GEN-SIM,LHE --conditions 106X_upgrade2023_realistic_v2 --beamspot HLLHC14TeV --step LHE,GEN,SIM \
+        --nThreads 8 \
+        --geometry Extended2023D41 --era Phase2C8_timing_layer_bar \
+        --python_filename lhe_cfg.py \
+        --no_exec \
+        --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${seed})" \
+        -n 100 \
+        || exit $? ;
