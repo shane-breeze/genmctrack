899fe9d37f0098f5044b01f6fb2afbb5b1b8801e
Shane Breeze <s.breeze15@imperial.ac.uk>
2020-03-19 17:20:30 +0000

    Update nanoaod setup

--- nanoaod_setup.sh
+++ nanoaod_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export SCRAM_ARCH=slc6_amd64_gcc700
+export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-if [ -r CMSSW_10_2_5/src ] ; then
-    echo release CMSSW_10_2_5 already exists
+if [ -r CMSSW_10_6_0_patch2/src ] ; then
+    echo release CMSSW_10_6_0_patch2 already exists
 else
-    scram p CMSSW CMSSW_10_2_5
+    scram p CMSSW CMSSW_10_6_0_patch2
 fi
-cd CMSSW_10_2_5/src
+cd CMSSW_10_6_0_patch2/src
 eval `scram runtime -sh`
 
 
@@ -15,10 +15,10 @@ cd ../../
 cmsDriver.py \
     --filein file:miniaod.root \
     --fileout file:nanoaod.root \
-    --mc --eventcontent NANOEDMAODSIM \
-    --datatier NANOAODSIM --conditions 106X_upgrade2023_realistic_v3 --step NANO \
+    --mc --eventcontent NANOAODSIM \
     --nThreads 8 \
-    --era Phase2C8_timing_layer_bar \
+    --datatier NANOAODSIM --conditions 106X_upgrade2023_realistic_v3 --step NANO \
+    --geometry Extended2023D41 --era Phase2C8_timing_layer_bar \
     --python_filename nanoaod_cfg.py \
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 100 \
     || exit $? ;
